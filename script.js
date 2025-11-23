2ea1e074d3b8cc0eac987a0813dbf51cc44e8536
Anupa <anupaadhikari09@gmail.com>
2025-11-23 15:52:23 +0545

    First commit

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const Element = React.createElement('h2', {className: 'sub-heading'}, 'Hello',
-    React.createElement('p',null,'This is a paragraph')
-)
+// Create a button click handler
+function handleClick() {
+    alert('Button clicked!');
+}
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+// Create a React element with nested elements and a button
+const Element = React.createElement(
+    'div',
+    { className: 'container', style: { textAlign: 'center', marginTop: '50px' } },
+    React.createElement('h2', { className: 'sub-heading' }, 'Hello, Anuu!'),
+    React.createElement('p', null, 'This is a paragraph. You can click the button below!'),
+    React.createElement(
+        'button',
+        { onClick: handleClick, style: { padding: '10px 20px', fontSize: '16px', cursor: 'pointer' } },
+        'Click Me'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement('li', null, 'Item 1'),
+        React.createElement('li', null, 'Item 2'),
+        React.createElement('li', null, 'Item 3')
+    )
+);
 
-root.render(Element)
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(Element);
